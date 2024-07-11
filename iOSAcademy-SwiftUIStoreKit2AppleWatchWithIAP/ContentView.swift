7f03afdbb5c1c1dc4964f6b1bc089494ab52e2e0
RainBowT0506 <rainbowt0506@gmail.com>
2024-07-11 15:12:05 +0800

    Initial Commit

---
+++ iOSAcademy-SwiftUIStoreKit2AppleWatchWithIAP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSAcademy-SwiftUIStoreKit2AppleWatchWithIAP
+//
+//  Created by RainBowT on 2024/7/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSAcademy-SwiftUIStoreKit2AppleWatchWithIAP/iOSAcademy_SwiftUIStoreKit2AppleWatchWithIAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAcademy_SwiftUIStoreKit2AppleWatchWithIAPApp.swift
+//  iOSAcademy-SwiftUIStoreKit2AppleWatchWithIAP
+//
+//  Created by RainBowT on 2024/7/11.
+//
+
+import SwiftUI
+
+@main
+struct iOSAcademy_SwiftUIStoreKit2AppleWatchWithIAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
